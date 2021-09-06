e754ae770e8ba4b2244b5af1d9c107a50281e707
JBabo <jbabo@protonmail.com>
2021-09-06 19:27:26 -0300

    update

--- archives/deletetask-function.js
+++ archives/deletetask-function.js
@@ -1,6 +1,6 @@
 function deleteTask(event) {                                                //chama o evento para apagar um item da lista
     let buttonTarget = event.target;                                        //cria uma variável que recebe o evento alvo de clique como valor
-    let listElement = document.getElementById('ul');                        //cria uma variável que recebe o elemento 'ul' do html como valor
+    let listElementDel = document.getElementById('ul');                        //cria uma variável que recebe o elemento 'ul' do html como valor
     let taskToDelete = buttonTarget.parentNode;                             //cria uma variável que recebe como valor o retorno da variável-pai 'buttonTarget'
-    listElement.removeChild(taskToDelete);                                  //coloca um método na variável 'listElement' para remover um elemento filho resultado da variável 'taskToDelete'
+    listElementDel.removeChild(taskToDelete);                                  //coloca um método na variável 'listElement' para remover um elemento filho resultado da variável 'taskToDelete'
 }
