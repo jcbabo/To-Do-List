505f1cea4cf4a71fb7e2aabb67713ce118238af6
JBabo <jbabo@protonmail.com>
2021-09-09 22:23:11 -0300

    update separaçao da funçao callback

--- archives/edittask-function.js
+++ archives/edittask-function.js
@@ -1,14 +1,20 @@
+
+
 function editTask(event) {
     let spanTarget = event.target;
     const entrada = document.createElement('input');
     entrada.value = spanTarget.innerText;
     spanTarget.style.display = 'none';
-    entrada.addEventListener('keydown', function(event){
-        if (event.key == "Enter") {
-            spanTarget.innerText = this.value;
-            spanTarget.style.display = 'inline';
-            this.style.display = 'none';
-        }
-    })
+    entrada.addEventListener('keydown', enterKey);
     spanTarget.parentNode.insertBefore(entrada, spanTarget);
+}
+
+function enterKey(event) {
+    let entrada = event.target;
+    let spanTarget = entrada.parentNode.querySelector('span');
+    if (event.key == "Enter") {
+        spanTarget.innerText = entrada.value;
+        spanTarget.style.display = 'inline';
+        entrada.style.display = 'none';
+    }
 }
