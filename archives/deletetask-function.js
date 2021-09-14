12649f7a98306c695ea5e275102d033ce1d44731
JBabo <jbabo@protonmail.com>
2021-09-14 14:59:26 -0300

    update para apagar o item do array de forma que a tarefa apagada realmente deixe de existir

--- archives/deletetask-function.js
+++ archives/deletetask-function.js
@@ -1,6 +1,9 @@
 function deleteTask(event) {                                                //chama o evento para apagar um item da lista
     let buttonTarget = event.target;                                        //cria uma variável que recebe o evento alvo de clique como valor
-    let listElementDel = document.getElementById('ul');                        //cria uma variável que recebe o elemento 'ul' do html como valor
+    let listElementDel = document.getElementById('ul');                     //cria uma variável que recebe o elemento 'ul' do html como valor
     let taskToDelete = buttonTarget.parentNode;                             //cria uma variável que recebe como valor o retorno da variável-pai 'buttonTarget'
-    listElementDel.removeChild(taskToDelete);                                  //coloca um método na variável 'listElement' para remover um elemento filho resultado da variável 'taskToDelete'
+    let spanTarget = buttonTarget.parentNode.querySelector('span').innerText;
+    let positionItem = list.indexOf(spanTarget);
+    list.splice(positionItem, 1);
+    listElementDel.removeChild(taskToDelete);                               //coloca um método na variável 'listElement' para remover um elemento filho resultado da variável 'taskToDelete'
 }
